8452b8b79f6d24bb683c8bbd376733f87614febf
Jing Li <lyovse@gmail.com>
2019-10-04 16:05:24 +1000

    Finish encryption

--- javascript/elgamal.js
+++ javascript/elgamal.js
@@ -8,6 +8,21 @@ function generateKey() {
     document.getElementById("pubkey").value = pubKey;
 }
 
+function encrypt() {
+    let p = getNumber("P");
+    let g = getNumber("G");
+    let y = getNumber("Y");
+    let m = getNumber("M");
+    let k = getNumber("k");
+    let c1 = fastExponentiation(g, k, p);
+    let K = fastExponentiation(y, k, p);
+    let c2 = (K * m) % p;
+    document.getElementById("C1").value = c1;
+    document.getElementById("C2").value = c2;
+    let cipher = `(${c1}, ${c2})`;
+    document.getElementById("cipher").value = cipher;
+}
+
 function getNumber(id) {
     return Number(document.getElementById(id).value)
 }
